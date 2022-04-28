3c45d163c6ef2bf01ec59b62cddc15a634027087
Dathin <pedromcaires99@gmail.com>
2022-04-28 03:27:38 -0300

    install scripts

---
+++ install-all.sh
@@ -0,0 +1,10 @@
+cd dathin-starter-errorhandling
+mvn clean install
+cd ../dathin-starter-model
+mvn clean install
+cd ../dathin-starter-openapi
+mvn clean install
+
+cd ../dathin-starter-dependencies
+mvn clean install
+
