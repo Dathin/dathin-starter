3a083af5a8521b5b080c709c4057082075123db5
Dathin <pedromcaires99@gmail.com>
2022-08-30 17:01:35 -0300

    User Details Autoconfiguration auto removal

--- dathin-archetype/src/main/resources/archetype-resources/__rootArtifactId__Service/src/main/java/__rootArtifactId__/Application.java
+++ dathin-archetype/src/main/resources/archetype-resources/__rootArtifactId__Service/src/main/java/__rootArtifactId__/Application.java
@@ -2,9 +2,8 @@ package ${package}.${rootArtifactId};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
